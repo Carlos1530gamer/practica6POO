5b61665ef72aa1dc3db3075530d62b53dce891d7
Carlos <carlos@gmail.com>
2019-03-12 12:38:36 -0600

    -iniciado el proyecto

---
+++ practica6/practica6/ButtonPer.swift
@@ -0,0 +1,24 @@
+//
+//  ButtonPer.swift
+//  practica6
+//
+//  Created by alumno on 3/12/19.
+//  Copyright © 2019 chauteco. All rights reserved.
+//
+
+import UIKit
+
+class ButtonPer: UIButton{
+//    override func awakeFromNib() {
+//        self.backgroundColor = .red
+//    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.backgroundColor = .red
+    }
+    
+    func move(){
+    }
+    
+}
---
+++ practica6/practica6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practica6
+//
+//  Created by alumno on 3/12/19.
+//  Copyright © 2019 chauteco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
